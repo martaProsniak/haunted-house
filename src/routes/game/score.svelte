1d6f1cea9fd2e2a1c16cd8a566de2bf83940eb3f
martaprosniak <martaprosniak@gmail.com>
2024-12-14 22:25:13 +0100

    Tweened store in score

--- src/routes/game/score.svelte
+++ src/routes/game/score.svelte
@@ -1,12 +1,18 @@
 <script lang="ts">
     import {score} from "./gameState.svelte";
-    import {fly, scale} from "svelte/transition";
+    import { tweened } from 'svelte/motion'
+
+    let currentScore = tweened($score)
+
+    $effect(() => {
+        currentScore.set($score);
+    })
 </script>
 
 <div class="">
     <div>Score:
         {#key $score}
-            <p class="text-3xl" in:scale={{duration: 400}}>{$score}</p>
+            <p class="text-3xl">{Math.trunc($currentScore)}</p>
         {/key}
     </div>
 </div>
