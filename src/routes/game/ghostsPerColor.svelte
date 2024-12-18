66ff26283eb3f7232b92c335940fce103f6495b5
Marta Kalwaj <martaprosniak@gmail.com>
2024-12-19 00:08:38 +0100

    Removed ghosts images from game info

--- src/routes/game/ghostsPerColor.svelte
+++ src/routes/game/ghostsPerColor.svelte
@@ -1,12 +1,7 @@
 <script lang="ts">
     import {gameStatus} from "./gameState.svelte.js";
     import {fade} from 'svelte/transition'
-    import blueGhost from '$lib/assets/ghost-blue.png';
-    import pinkGhost from '$lib/assets/ghost-pink.png';
-    import greenGhost from '$lib/assets/ghost-green.png';
-    import {groupGhostsPerColor} from "./utils";
     import type {GhostSummary} from "./types";
-    import Badge from './badge.svelte'
 
     interface Props {
         ghosts: GhostSummary
@@ -14,25 +9,16 @@
 
     const {ghosts}: Props = $props();
 
-    let ghostsPerColor: Record<string, number> = $derived.by(() => groupGhostsPerColor(ghosts));
-
-    const ghostImages: Record<string, string> = {
-        blue: blueGhost,
-        pink: pinkGhost,
-        green: greenGhost,
-    }
+    let count = $derived(Object.keys(ghosts).length)
 
 </script>
 
 {#if $gameStatus !== 'not-started'}
     <div class="h-14" in:fade={{duration: 200}}>
         <div class="flex gap-x-8 w-fit items-center">
-            {#each Object.keys(ghostsPerColor) as color}
-                <div class="relative">
-                    <img class="w-full" src={ghostImages[color]} alt={color}>
-                        <Badge count={ghostsPerColor[color]} color={color}/>
-                </div>
-            {/each}
+            {#key count}
+                    <div class="w-5" transition:fade|local={{duration: 300}}>{count}</div>
+            {/key}
         </div>
     </div>
 {/if}
