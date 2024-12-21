fffeea325bbc3385f44e8eee4f3b27147a62f9b6
Marta Kalwaj <martaprosniak@gmail.com>
2024-12-21 16:06:01 +0100

    Pause modal

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -2,11 +2,13 @@
     import Game from './game.svelte';
     import WelcomeModal from "./welcomeModal.svelte";
     import EndLevel from "./endLevel.svelte";
+    import Pause from "./pause.svelte";
 </script>
 
 
 <WelcomeModal />
 <EndLevel />
+<Pause />
 <div class="container mx-auto">
     <Game />
 </div>
---
+++ src/routes/game/pause.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+   import {isPaused} from "./gameState.svelte";
+   import {togglePause} from "./gameStateH.handlers.svelte";
+   import {fly} from "svelte/transition";
+</script>
+
+{#if $isPaused}
+    <dialog class="p-20 bg-violet-300 text-violet-500" open transition:fly={{duration: 200, y: -100}}>
+        <button onclick={togglePause}>Play [space]</button>
+    </dialog>
+{/if}
+
+<style>
+    dialog {
+        z-index: 50;
+    }
+</style>
