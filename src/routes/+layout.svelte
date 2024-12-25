e10b692963e2e216bbea71b8c0617a0fb6db982e
martaprosniak <martaprosniak@gmail.com>
2024-12-25 19:12:59 +0100

    Guide in progress

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,6 @@
     const {children} = $props();
 </script>
 
-<div class="text-violet-200 h-screen w-screen font-nunito flex items-center justify-center py-16">
+<div class="text-violet-200 h-screen w-screen font-nunito flex items-center justify-center py-16 max-h-screen overflow-hidden">
     {@render children()}
 </div>
--- src/routes/guide/+page.svelte
+++ src/routes/guide/+page.svelte
@@ -1,4 +1,67 @@
 <script lang="ts">
 </script>
 
-<div class="self-start">Rules</div>
+<main class="place-self-start py-8 px-16 semi-transparent w-2/3 max-h-full overflow-y-scroll rounded-lg space-y-6 text-base">
+    <h1 class="text-2xl text-center">Ghosthunters guide</h1>
+    <div class="space-y-2">
+        <h2 class="text-xl">How to catch a ghost?</h2>
+        <p>There are three kind of ghosts: pink, blue and green. You need to match them with at least 3
+            plasma with the same color.</p>
+        <p>But be careful! Ghosts are trying to escape!</p>
+
+    </div>
+    <div class="space-y-2">
+        <h2 class="text-xl">What? Escape?</h2>
+        <p>Yeah, did you think they will just wait until you catch them? Oh no, not these ones.</p>
+        <p>But don't worry, we figured them out!</p>
+        <p>They move one by one. Their primary direction is always up, unless you block their way with a plasma. In that
+            case they will attempt to go right, and if it also won't be possible, they will try to move left. You need
+            to block them as soon as you can!</p>
+        <p>If you allow three ghosts to escape, they will attack you and you'll have to start over!</p>
+    </div>
+    <div>
+        <p>Here's some good news - if you place matching plasma next to them, they will loose ability to move! Plasma is
+            veeery sticky.</p>
+    </div>
+    <div class="space-y-2">
+        <h2 class="text-xl">Your weapon</h2>
+        <p>You will be provided with special Spooky Plasma Shooter! It's filled with two-cell plasma bullets. Released
+            bullet will move down in interval. If the bullet will collide with an obstacle, it will break in half and leave two plasmas
+            on the floor.</p>
+        <p>Watch out! As plasma is immaterial, every loose molecule will go down until it'll hit the obstacle.</p>
+    </div>
+    <div class="space-y-2">
+        <h2 class="text-xl">Steering bullets</h2>
+        <p>You steer bullets with keyboard (Spooky Plasma Shooter is compatible with any keyboard!). Use arrows to move
+            the bullets across the house.</p>
+    </div>
+</main>
+
+<style>
+    .semi-transparent {
+        background-color: rgba(11, 7, 22, 0.75);
+    }
+
+    ::-webkit-scrollbar {
+        width: 10px;
+    }
+
+    ::-webkit-scrollbar-track {
+        background: rgba(11, 7, 22, 0.75);
+    }
+
+    ::-webkit-scrollbar-thumb {
+        background-color: black;
+        border-radius: 10px;
+        border: 3px solid rgba(11, 7, 22, 0.75);
+    }
+
+    ::-webkit-scrollbar-thumb:hover {
+        background: black;
+    }
+
+    main {
+        scrollbar-width: thin;
+        scrollbar-color: black rgba(11, 7, 22, 0.75);
+    }
+</style>
