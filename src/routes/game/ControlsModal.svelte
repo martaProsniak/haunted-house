defda88bd468c664ec56fd9039353f4910039116
Marta Kalwaj <martaprosniak@gmail.com>
2025-01-09 22:44:48 +0100

    Close control modal when game is unpaused from outside

--- src/routes/game/ControlsModal.svelte
+++ src/routes/game/ControlsModal.svelte
@@ -1,14 +1,22 @@
 <script lang="ts">
     import KeyboardManual from './KeyboardManual.svelte';
     import {fly} from 'svelte/transition'
+    import {isPaused} from "./gameState.svelte";
 
     let {open, handleClose} = $props();
+
+    $effect(() => {
+        if ($isPaused) {
+            return;
+        }
+        handleClose();
+    })
 </script>
 
 {#if open}
-    <dialog class="h-full w-full text-xl px-10 py-20 text-violet-200 bg-darkViolet" {open} in:fly={{duration: 200, y: -200}} out:fly={{duration: 300, y: -200}}>
+    <dialog class="h-full w-full text-xl px-10 py-20 flex items-center justify-center text-blue-200 bg-darkViolet" {open} in:fly={{duration: 200, y: -200}} out:fly={{duration: 300, y: -200}}>
         <KeyboardManual />
-        <button class="p-2 px-4 outline-violet-600 rounded-lg cursor-pointer absolute top-4 right-4" onclick={handleClose}>
+        <button class="text-right py-4 px-6 outline-violet-600 hover:bg-black cursor-pointer rounded-bl-lg absolute top-0 right-0" onclick={handleClose}>
             X
         </button>
     </dialog>
--- src/routes/game/PauseModal.svelte
+++ src/routes/game/PauseModal.svelte
@@ -7,8 +7,8 @@
 </script>
 
 {#if $isPaused}
-    <dialog class="py-20 px-36  flex items-center justify-center h-full w-full text-xl" open={$isPaused} transition:fly={{duration: 300, y: -200}}>
-        <button onclick={togglePause} class="cursor-pointer text-violet-200 hover:text-violet-300 hover:rotate-2 transition-all">
+    <dialog class="py-20 px-36 bg-semi-transparent flex items-center justify-center text-blue-200 h-full w-full text-xl" open={$isPaused} transition:fly={{duration: 300, y: -200}}>
+        <button onclick={togglePause} class="cursor-pointer  hover:text-blue-300 hover:rotate-2 transition-all">
             <span class="inline-block mx-auto"><PlayIcon /></span>
             <span>Resume</span>
             <span class="mx-auto flex items-center justify-center">
@@ -23,6 +23,6 @@
 <style>
     dialog {
         z-index: 40;
-        background-color: rgba(1, 1, 2, 0.74);
+        /*background-color: rgba(1, 1, 2, 0.74);*/
     }
 </style>
