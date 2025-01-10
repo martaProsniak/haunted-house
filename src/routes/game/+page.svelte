da23d3fce615606dcc8b780f31dbeb994f1e8156
martaprosniak <martaprosniak@gmail.com>
2025-01-10 19:22:53 +0100

    Preloading important assets

--- src/routes/game/+page.svelte
+++ src/routes/game/+page.svelte
@@ -4,6 +4,20 @@
     import {gameStatus} from "./gameState.svelte";
     import {setLocalData} from "./gameState.helpers.svelte";
     import {onMount} from "svelte";
+    import pinkGhostGif from "$lib/assets/ghost-pink.gif";
+    import blueGhostGif from "$lib/assets/ghost-blue.gif";
+    import greenGhostGif from "$lib/assets/ghost-green.gif";
+    import explosionImage from "$lib/assets/explosion.gif";
+    import plasmaPinkPink from '$lib/assets/flying-pink-pink.png';
+    import plasmaPinkBlue from '$lib/assets/flying-pink-blue.png';
+    import plasmaPinkGreen from '$lib/assets/flying-pink-green.png';
+    import plasmaBluePink from '$lib/assets/flying-blue-pink.png';
+    import plasmaBlueBlue from '$lib/assets/flying-blue-blue.png';
+    import plasmaBlueGreen from '$lib/assets/flying-blue-green.png';
+    import plasmaGreenPink from '$lib/assets/flying-green-pink.png';
+    import plasmaGreenBlue from '$lib/assets/flying-green-blue.png';
+    import plasmaGreenGreen from '$lib/assets/flying-green-green.png';
+
 
     let showGame = $derived($gameStatus !== 'not-started');
 
@@ -19,6 +33,21 @@
     })
 </script>
 
+<svelte:head>
+    <link rel="preload" href={pinkGhostGif} as="image" type="image/gif" />
+    <link rel="preload" href={blueGhostGif} as="image" type="image/gif" />
+    <link rel="preload" href={greenGhostGif} as="image" type="image/gif" />
+    <link rel="preload" href={explosionImage} as="image" type="image/gif" />
+    <link rel="preload" href={plasmaPinkGreen} as="image" type="image/gif" />
+    <link rel="preload" href={plasmaPinkBlue} as="image" type="image/gif" />
+    <link rel="preload" href={plasmaPinkPink} as="image" type="image/gif" />
+    <link rel="preload" href={plasmaBlueGreen} as="image" type="image/gif" />
+    <link rel="preload" href={plasmaBlueBlue} as="image" type="image/gif" />
+    <link rel="preload" href={plasmaBluePink} as="image" type="image/gif" />
+    <link rel="preload" href={plasmaGreenBlue} as="image" type="image/gif" />
+    <link rel="preload" href={plasmaGreenGreen} as="image" type="image/gif" />
+    <link rel="preload" href={plasmaGreenPink} as="image" type="image/gif" />
+</svelte:head>
 
 {#if showGame}
     <Game/>
